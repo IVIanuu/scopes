686929c2e873566d59c8d6de08125945c5801c46
IVIanuu <IVIanuu@gmail.com>
2019-05-05 16:01:46 +0200

    Added missing fragment function

--- scopes-android/src/main/java/com/ivianuu/scopes/android/fragment.kt
+++ scopes-android/src/main/java/com/ivianuu/scopes/android/fragment.kt
@@ -19,13 +19,10 @@ package com.ivianuu.scopes.android
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.Lifecycle
 import com.ivianuu.scopes.Scope
-import com.ivianuu.scopes.android.onCreate
-import com.ivianuu.scopes.android.onDestroy
-import com.ivianuu.scopes.android.onPause
-import com.ivianuu.scopes.android.onResume
-import com.ivianuu.scopes.android.onStart
-import com.ivianuu.scopes.android.onStop
-import com.ivianuu.scopes.android.scopeFor
+import com.ivianuu.scopes.lifecycle.LifecycleScopes
+
+val Fragment.viewLifecycleScopes: LifecycleScopes<Lifecycle.Event>
+    get() = viewLifecycleOwner.lifecycleScopes
 
 fun Fragment.viewScopeFor(event: Lifecycle.Event): Scope = viewLifecycleOwner.scopeFor(event)
 
