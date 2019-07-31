e86c9f375bcc97966b78cddbbe5f514c5ddf9e26
IVIanuu <ivianuu@gmail.com>
2019-08-01 00:15:20 +0200

    ..

--- buildSrc/src/main/kotlin/dependencies.kt
+++ buildSrc/src/main/kotlin/dependencies.kt
@@ -28,7 +28,7 @@ object Build {
 object Publishing {
     const val groupId = "com.ivianuu.scopes"
     const val vcsUrl = "https://github.com/IVIanuu/scopes"
-    const val version = "${Build.versionName}-dev13"
+    const val version = "${Build.versionName}-dev14"
 }
 
 object Versions {
